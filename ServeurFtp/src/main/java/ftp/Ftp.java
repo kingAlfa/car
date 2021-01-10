c6cf9db770f7e05f728f3528aba740cf1bc0bc34
barrym <mamadou.barry2.etu@univ-lille.fr>
2021-01-10 22:06:47 +0100

    Creation du projet

---
+++ ServeurFtp/src/main/java/ftp/Ftp.java
@@ -0,0 +1,11 @@
+package ftp;
+
+import java.net.Socket;
+
+public class Ftp {
+    private String user;
+    private Socket socket = null,socketData = null;
+    private boolean DEBUG = true;
+    private String host;
+    private int port;
+}
