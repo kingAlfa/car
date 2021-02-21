37e8db98c22a90edf4c95df2cce56cd18d0f880b
Alpha <49416318+kingAlfa@users.noreply.github.com>
2021-02-21 01:19:14 +0100

    Delete TpEcom
    Projet abandonné

--- TpEcom/src/main/java/fil/car/projet/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package fil.car.projet.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fil.car.projet.model.Client;
-
-public interface ClientRepository extends CrudRepository<Client,Integer>{
-
-}
--- TpEcom/src/main/java/fil/car/projet/repository/ProduitRepository.java
+++
@@ -1,9 +0,0 @@
-package fil.car.projet.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fil.car.projet.model.Produit;
-
-public interface ProduitRepository extends CrudRepository<Produit,Integer>{
-
-}
